8d10189d010e68f135ac034c00b348a9fa63e5a7
JuanmaLop <juan.ocampo4@udea.edu.co>
2025-10-20 17:28:58 -0500

    Cambios proyecto Base

---
+++ src/test/java/com/udeateampro/CotSys/CotSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udeateampro.CotSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
